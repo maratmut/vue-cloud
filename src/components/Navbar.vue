b9c87d95ffd5bd6ce91027f28388b4f867ed87bf
Marat Mutalipov <maratios41@gmail.com>
2023-05-04 13:23:20 +0300

    Create and register all icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 <template>
   <nav class="navbar navbar-light bg-white navbar-expand-lg border-bottom fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="/"><strong>Vue</strong>Drive</a>
+      <a class="navbar-brand" href="/"><strong>Vue</strong> Cloud</a>
 
       <form class="form-inline mt-2 mt-md-0">
         <input class="form-control" type="text" placeholder="Search in Drive" aria-label="Search" />
