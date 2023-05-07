48254681f5b1d7ac1502e4394a66d33f5262f931
Marat Mutalipov <maratios41@gmail.com>
2023-05-07 14:31:43 +0300

    Adding sorting feature to our application

---
+++ src/components/SortToggler.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive } from 'vue';
+
+const emit = defineEmits(['sort-change']);
+
+const sort = reactive({
+  column: 'name',
+  order: 'asc',
+});
+
+const toggle = () => {
+  sort.order = sort.order === 'asc' ? 'desc' : 'asc';
+  emit('sort-change', sort);
+};
+
+</script>
+<template>
+  <button @click="toggle" class="rounded-button">
+    <icon-arrow-up v-if="sort.order === 'asc'" />
+    <icon-arrow-down v-else />
+  </button>
+</template>
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -2,23 +2,33 @@
 import ActionBar from '../components/ActionBar.vue';
 import filesApi from '../api/files';
 import FilesList from '../components/files/FilesList.vue';
-import { onMounted, ref } from 'vue';
+import {ref, reactive, watchEffect } from 'vue';
+import SortToggler from '../components/SortToggler.vue';
 
 const files = ref([]);
 
-const fetchFiles = async () => {
+const query = reactive({
+  _sort: 'name',
+  _order: 'asc',
+});
+
+const handleSortChange = (payload) => {
+  query._sort = payload.column;
+  query._order = payload.order;
+};
+
+const fetchFiles = async (query) => {
   try {
-    const { data } = await filesApi.index();
+    const { data } = await filesApi.index(query);
     return data;
   } catch (error) {
     console.error(error);
   }
 };
 
-onMounted(async () => {
-  files.value = await fetchFiles();
+watchEffect(async () => {
+  files.value = await fetchFiles(query);
 });
-
 </script>
 <template>
   <div class="container py-3">
@@ -26,9 +36,7 @@ onMounted(async () => {
 
     <div class="d-flex justify-content-between align-items-center py-2">
       <h6 class="text-muted mb-0">Files</h6>
-      <button class="rounded-button">
-        <icon-arrow-up />
-      </button>
+      <SortToggler @sort-change="handleSortChange($event)" />
     </div>
     <FilesList :files="files" />
   </div>
