601972a6d602fcedfa651233d700c1ceb8ee3161
Marat Mutalipov <maratios41@gmail.com>
2023-05-11 16:27:56 +0300

    Remove the selected files. Create a toast notification

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import icons from "./components/icons/all";
+import { createApp } from 'vue';
+import App from './App.vue';
+import icons from './components/icons/all';
+import Toast from './components/toast/Toast.vue';
 
 const app = createApp(App);
 
 // Register all icons
 for (const [key, value] of Object.entries(icons)) {
-    app.component(key, value);
-  }
+  app.component(key, value);
+}
 
-app.mount('#app')
+app.component('app-toast', Toast);
+
+app.mount('#app');
--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -7,6 +7,10 @@ import SortToggler from '../components/SortToggler.vue';
 import SearchForm from '../components/SearchForm.vue';
 
 const files = ref([]);
+const toast = reactive({
+  show: false,
+  message: '',
+});
 
 const query = reactive({
   _sort: 'name',
@@ -36,13 +40,34 @@ const fetchFiles = async (query) => {
   }
 };
 
+const removeItem = async (item, files) => {
+  try {
+    const response = await filesApi.delete(item.id);
+    if (response.status === 200 || response.status === 204) {
+      const index = files.value.findIndex((file) => file.id === item.id);
+      files.value.splice(index, 1);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleRemove = () => {
+  if (confirm('Вы уверены что хотите удалить?')) {
+    selectedItems.value.forEach((item) => removeItem(item, files));
+    selectedItems.value.splice(0);
+    toast.show = true;
+    toast.message = 'Успешно удалено';
+  }
+};
+
 watchEffect(async () => {
   files.value = await fetchFiles(query);
 });
 </script>
 <template>
   <div class="container py-3">
-    <ActionBar :selected-count="selectedItems.length" />
+    <ActionBar :selected-count="selectedItems.length" @remove="handleRemove" />
 
     <div class="d-flex justify-content-between align-items-center py-2">
       <h6 class="text-muted mb-0">Файлы</h6>
@@ -52,6 +77,13 @@ watchEffect(async () => {
       <SearchForm v-model="q" />
     </teleport>
     <FilesList :files="files" @select-change="handleSelectChange($event)" />
+    <app-toast
+      :show="toast.show"
+      :message="toast.message"
+      type="Отлично"
+      position="bottom-left"
+      @hide="toast.show = false"
+    ></app-toast>
   </div>
 </template>
 
